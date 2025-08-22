feda80dfe19b042186c3f2b3034aff6e520c8f71
Shreyash Pise <125645116+Shreyash0711@users.noreply.github.com>
2025-08-22 09:55:31 +0530

    Delete Book Info Sys directory

--- Book Info Sys/main.py
+++
@@ -1,5 +0,0 @@
-from python_files.app import  App
-import tkinter as tk
-if __name__ == "__main__":
-    root = App()
-    root.mainloop()
--- Book Info Sys/python_files/button.py
+++
@@ -1,5 +0,0 @@
-import tkinter as tk
-class ButtonClass:
-    def __init__(self, master, text, row, col, command, padx=5, pady=5):
-        self.button = tk.Button(master, text=text, font=("Arial", 12), bg="lightblue", command=command)
-        self.button.grid(row=row, column=col, padx=padx, pady=pady, sticky="ew")
